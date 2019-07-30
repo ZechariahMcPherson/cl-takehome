8c37c84748878cda1d47cf76aee38b240a8828cd
G. Zechariah McPherson <ZechariahMcPherson@gmail.com>
2019-07-30 16:41:25 -0400

    initial bird cat filter working

--- main.js
+++ main.js
@@ -5,8 +5,6 @@ const handleDropdownMenuItemClick = () => {
     "dropdownMenuButtonArrow"
   );
 
-  console.log(dropdownMenuButtonArrow);
-
   if (!!dropdownMenu.classList.contains("display-block")) {
     dropdownMenuButton.classList.remove("dropdown-button-selected");
     dropdownMenu.classList.remove("display-block");
@@ -16,4 +14,54 @@ const handleDropdownMenuItemClick = () => {
     dropdownMenu.classList.add("display-block");
     dropdownMenuButtonArrow.classList.add("downward-arrow");
   }
+};
+
+const wantToSeeOnClick = animal => {
+  const wantToSeeCards = document.getElementsByClassName("want-to-see-card");
+
+  const birdsCheckbox = document.getElementById("birdsCheckbox");
+  const catsCheckbox = document.getElementById("catsCheckbox");
+
+  // neither or both checkboxes are checked = show both birds and cats
+  if (
+    (!birdsCheckbox.checked && !catsCheckbox.checked) ||
+    (birdsCheckbox.checked && catsCheckbox.checked)
+  ) {
+    seeAllCards(wantToSeeCards);
+  }
+  // user wants to see birds
+  else if (birdsCheckbox.checked) {
+    seeBirds(wantToSeeCards);
+  }
+
+  // user wants to see cats
+  else if (catsCheckbox.checked) {
+    seeCats(wantToSeeCards);
+  }
+};
+
+const seeAllCards = cards => {
+  Object.values(cards).map(card => {
+    card.classList.remove("display-none");
+  });
+};
+
+const seeBirds = cards => {
+  Object.values(cards).map(card => {
+    if (card.classList.contains("bird-card")) {
+      card.classList.remove("display-none");
+    } else if (card.classList.contains("cat-card")) {
+      card.classList.add("display-none");
+    }
+  });
+};
+
+const seeCats = cards => {
+  Object.values(cards).map(card => {
+    if (card.classList.contains("cat-card")) {
+      card.classList.remove("display-none");
+    } else if (card.classList.contains("bird-card")) {
+      card.classList.add("display-none");
+    }
+  });
 };
